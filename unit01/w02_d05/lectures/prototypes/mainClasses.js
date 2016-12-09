4046fa33d774635c57c205700a274a84b65dcbe7
vga <vgabruzzo@gmail.com>
2016-12-09 09:34:02 -0500

    edit prototypes lecture

--- unit01/w02_d05/lectures/prototypes/mainClasses.js
+++ unit01/w02_d05/lectures/prototypes/mainClasses.js
@@ -9,37 +9,37 @@ class Animal {
   }
 }
 
-const george = new Animal('george', true);
+// const george = new Animal('george', true);
+//
+// console.log(george);
+// george.sayName();
 
-console.log(george);
-george.sayName();
+// class Dog extends Animal {
+//   constructor(name, isAlive, breed) {
+//     super(name, isAlive);
+//
+//     this.breed = breed;
+//   }
+//
+//   bark() {
+//     if (this.isAlive === true) {
+//       console.log('woof woof woof');
+//     } else {
+//       console.log('woof woof woof... from heaven');
+//     }
+//   }
+// }
+//
+// const jack = new Dog('jack', true, 'pitbull');
+// const jill = new Dog('jill', false, 'puggle')
 
-class Dog extends Animal {
-  constructor(name, isAlive, breed) {
-    super(name, isAlive);
-
-    this.breed = breed;
-  }
-
-  bark() {
-    if (this.isAlive === true) {
-      console.log('woof woof woof');
-    } else {
-      console.log('woof woof woof... from heaven');
-    }
-  }
-}
-
-const jack = new Dog('jack', true, 'pitbull');
-const jill = new Dog('jill', false, 'puggle')
-
-console.log(jack);
-console.log(jill);
-
-jack.sayName();
-jack.bark();
-console.log(jack.breed);
-
-jill.sayName();
-jill.bark();
-console.log(jill.breed);
+// console.log(jack);
+// console.log(jill);
+//
+// jack.sayName();
+// jack.bark();
+// console.log(jack.breed);
+//
+// jill.sayName();
+// jill.bark();
+// console.log(jill.breed);
