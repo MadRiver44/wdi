6ebc7706350a52ba7fb41aa76c380619d1051f1b
shishkina <shishkina.kate@gmail.com>
2016-12-09 16:31:32 -0500

    added weekend homework

---
+++ unit01/w02_d05/homework/hangman_starter/src/letter.js
@@ -0,0 +1,14 @@
+console.log('letter.js is connected');
+
+class Letter {
+  constructor() {
+    //Put attributes here!
+  }
+  show() {
+    //What abilites go in the show method?
+  }
+  render() {
+    //How do you return '_' if the letter is hidden
+    //And the letter if it's showing?
+  }
+}
