e23a5886143d7ff38848af2a510ebf339ee53e79
arianadziedzic <ariana.dziedzic@gmail.com>
2016-12-19 14:10:00 -0500

    Unit Two Squad Breakdown

--- unit01/w02_d04/morning_exercise/jasmine_calculator_starter/calculator.js
+++ unit01/w02_d04/morning_exercise/jasmine_calculator_starter/calculator.js
@@ -6,6 +6,33 @@ class Calculator {
   }
 
   subtract(num1, num2) {
-
+		return num1 - num2;
   }
+
+	multiply(num1, num2) {
+		return num1 * num2;
+	}
+
+	divide(a,b){
+		return a/b;
+	}
+	square(num1){
+		return Math.pow(num1,2);
+	}
+
+	exponential(num1,num2) {
+		return Math.pow(num1, num2);
+	}
+
+	isGreaterThan(num1,num2) {
+		if (num1 > num2) return num1;
+		else return num2;
+	}
+
+	isNegative(num){
+		if (num<0){
+			return true;
+		}
+		else return false;
+	}
 }
