125daf142fb8c0119f2a4733df7afe2c057cef9f
Patrick Andre <patrickandre@megatron.local>
2016-12-20 11:34:15 -0500

    lecture-lab-files

--- unit02/w04_d02/lab/ajax/demo.js
+++ unit02/w04_d02/lab/ajax/demo.js
@@ -2,13 +2,13 @@ let count = 1;
 
 function getData(num) {
   $.get(`http://swapi.co/api/people/${num}/`, function(data) {
-    $('#dataview').html('<p> Name: ' + data.name + '</p>');
+   /* $('#dataview').html('<p> Name: ' + data.name + '</p>');
      $('#dataview').append('<p>Eye Color : ' + data.eye_color + '</p>');
      $('#dataview').append('<p> Gender: ' + data.gender + '</p>');
-    $('#dataview').html('');
-    /*$.each(data, function(key, value) {
+    $('#dataview').html('');*/
+    $.each(data, function(key, value) {
       $('#dataview').append(`<p><strong>${key}</strong>: ${value}</p>`);
-    });*/
+    });
     $('#counter').text(`Person: ${count}`);
     console.log(data)
   });
