300c726a4550d7eba670986af380d41c55471d63
shishkina <shishkina.kate@gmail.com>
2016-12-21 09:01:52 -0500

    snowman pushed

---
+++ unit02/w04_d03/morning_exercise/jasmine_snowman_starter/spec/snowman_spec.js
@@ -0,0 +1,12 @@
+// IA's will walk through the creation of the snowman_spec file
+
+//I want my snowman builder to create a snowman object
+//My winter wonderland is a friendly place, so I want each snowman to have a name
+//In order for it to really be a snowman, it needs to have some fatures:
+//-a carrot nose.
+//-stick arms.
+//Our snowman should like warm hugs.
+
+describe('Snowman', () => {
+
+});
