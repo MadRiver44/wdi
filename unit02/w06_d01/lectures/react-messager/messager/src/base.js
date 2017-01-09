d2fed8e743445c22f70654a4ce85212a6099d386
vga <vgabruzzo@gmail.com>
2017-01-09 09:57:01 -0500

    add messager lab/lecture

---
+++ unit02/w06_d01/lectures/react-messager/messager/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyCOpzONb5mXdQ4STQXccbGKIkRiPHSQBeE",
+  authDomain: "messageboard-vince.firebaseapp.com",
+  databaseURL: "https://messageboard-vince.firebaseio.com",
+});
+
+export default base;
