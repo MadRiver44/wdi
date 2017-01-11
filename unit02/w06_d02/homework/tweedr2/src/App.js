5a9258bc5f647ca33be68bd3531812c9f452317f
Franklin Brooks <franklinchristopherbrooks@gmail.com>
2017-01-11 12:58:49 -0500

    added footer and tweetfeed components

--- unit02/w06_d02/homework/tweedr2/src/App.js
+++ unit02/w06_d02/homework/tweedr2/src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
+import Footer from './Footer';
+import TweedFeed from './TweedFeed';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-
-          <h2>Welcome to React</h2>
+          <h2>Tweedr2</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+          <TweedFeed />
+        </div>
+        <Footer />
       </div>
     );
   }
---
+++ unit02/w06_d02/homework/tweedr2/src/TweedFeed.js
@@ -0,0 +1,11 @@
+import React, { Component, PropTypes } from 'react';
+class TweedFeed extends Component {
+  render() {
+    return (
+      <div className='tweedfeed'>Tweeds go here
+      </div>
+    );
+  }
+}
+
+export default TweedFeed;
