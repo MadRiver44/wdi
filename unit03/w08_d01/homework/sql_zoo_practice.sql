f77c298d9ebd3cdb3e4232fb12e3f28881f27e16
Franklin Brooks <franklinchristopherbrooks@gmail.com>
2017-01-24 06:47:16 -0500

    sql_zoo_practice.sql

---
+++ unit03/w08_d01/homework/sql_zoo_practice.sql
@@ -0,0 +1,40 @@
+--1
+SELECT id, title
+ FROM movie
+ WHERE yr=1962;
+
+--2
+SELECT yr
+FROM movie
+WHERE title='Citizen Kane';
+
+--3
+
+--4
+
+--5
+
+--6
+SELECT id
+FROM movie
+WHERE title='Casablanca';
+
+--7
+
+--8
+
+--9
+
+--10
+
+--11
+
+--12
+
+--13
+
+--14
+
+--15
+
+--16
