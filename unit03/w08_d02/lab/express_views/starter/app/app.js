1b0f7fd676072461b9940f1e7ff6586d1f50263d
Joe Keohah <jkeohan@gmail.com>
2017-01-24 09:58:11 -0500

    jk

---
+++ unit03/w08_d02/lab/express_views/starter/app/app.js
@@ -0,0 +1,27 @@
+///////////////////////////////
+///// requires   //////////////
+//////////////////////////////
+// require the express module
+var express; 
+// set app to express
+var app;
+//////////////////////////////
+///// app.set() //////////////
+//////////////////////////////
+// .set() port to variable to use existing PORT global \\ 3000
+app.set();
+// .set() the view engine to ejs
+app.set()
+// .set() the view to app/views
+app.set()
+//////////////////////////////
+///// app.use() //////////////
+//////////////////////////////
+// .use() the static() middleware to use app/public folder
+app.use())
+// configure a the default route '/' to return '<h1>Home Page</h1>'
+app.get()
+//configure our server to listen on the 'port' variable defined earlier
+// have the server console.log that it's listening on that port
+var server;
+
