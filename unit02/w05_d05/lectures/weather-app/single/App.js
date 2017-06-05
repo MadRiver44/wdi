d344692340661b09411ddc3f06759b163b17eff1
Franklin Brooks <franklinchristopherbrooks@gmail.com>
2017-06-05 19:54:39 -0400

    vince socket io lecture

--- unit02/w05_d05/lectures/weather-app/single/App.js
+++ unit02/w05_d05/lectures/weather-app/single/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -46,7 +45,6 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to my React Weather App: built with a single component</h2>
         </div>
         <div className="weather-app-container">
